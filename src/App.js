34eab8c05bd39165cf2215b747297a21d1407bf1
Sam Blissard <samblissard@gmail.com>
2019-04-10 18:15:18 -0400

    Added weekly forecast component with fake data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,54 @@
 import React, { Component } from 'react';
+import WeeklyForecast from './components/WeeklyForecast'
 
 class App extends Component {
+  state = {
+    forecasts: [
+      {
+        day: 'Sunday',
+        high: 80,
+        low: 60
+      },
+      {
+        day: 'Monday',
+        high: 80,
+        low: 60
+      },
+      {
+        day: 'Tuesday',
+        high: 80,
+        low: 60
+      },
+      {
+        day: 'Wednesday',
+        high: 80,
+        low: 60
+      },
+      {
+        day: 'Thursday',
+        high: 80,
+        low: 60
+      },
+      {
+        day: 'Friday',
+        high: 80,
+        low: 60
+      },
+      {
+        day: 'Saturday',
+        high: 80,
+        low: 60
+      }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
         </header>
+        <WeeklyForecast forecasts={this.state.forecasts}></WeeklyForecast>
+        
       </div>
     );
   }
