f951bf7eabdcef897b1daa7527cfa3a39c7cdd6c
Sam Blissard <samblissard@gmail.com>
2019-04-10 18:23:02 -0400

    Show high/low in daily forecast

--- src/components/DailyForecast.js
+++ src/components/DailyForecast.js
@@ -2,8 +2,12 @@ import React, { Component } from 'react'
 
 export class DailyForecast extends Component {
   render() {
+    const { day, high, low } = this.props.forecast
+
     return (
-      <div>{this.props.forecast.day}</div>
+      <div>
+        {day} | {high} | {low}
+      </div>
     )
   }
 }
