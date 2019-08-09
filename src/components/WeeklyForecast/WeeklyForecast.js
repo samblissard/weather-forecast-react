9488e1805bec6b181e2297f7ec4ae895f4d5df36
samblissard <samblissard@gmail.com>
2019-08-09 14:01:24 -0400

    Moving weather cards to Grid

--- src/components/WeeklyForecast/WeeklyForecast.js
+++ src/components/WeeklyForecast/WeeklyForecast.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react'
 import DailyForecast from '../DailyForecast/DailyForecast'
 import PropTypes from 'prop-types'
+import Grid from '@material-ui/core/Grid'
 
 export class WeeklyForecast extends Component {
   render() {
     return (
-      this.props.forecasts.map(forecast => (
-        <DailyForecast key={forecast.day} forecast={forecast}></DailyForecast>
-      ))
+      <Grid container spacing={2}>
+        {this.props.forecasts.map(forecast => (
+          <Grid item xs={1}>
+            <DailyForecast key={forecast.day} forecast={forecast}></DailyForecast>
+          </Grid>
+        ))}
+      </Grid>
+        
     )
   }
 }
